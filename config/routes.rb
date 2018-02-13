b5bee2bc38ec62dd5bbc7cbc7587bb58fc918561
Hoon Lee <yl.hoony@gmail.com>
2018-02-13 16:34:40 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+  
 end
